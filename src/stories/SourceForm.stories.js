6f00e63fbad472e16c3a3f0e8ade996c0727bf4f
Jamie <james_wooltorton@hotmail.com>
2020-06-08 08:33:32 +0100

    patching

---
+++ src/stories/SourceForm.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SourceForm from '../components/sources/SourceForm';
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+
+addDecorator((story) => (
+  <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+));
+
+export default {
+  component: SourceForm,
+  title: 'SourceForm',
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+};
+
+export const sourceData = {
+  id: 1,
+  name: 'Test',
+  description: 'Test description',
+  endpoint: 'service-endpoint',
+};
+
+export const Basic = () => <SourceForm source={sourceData} />;
